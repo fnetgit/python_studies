828341afa7d96ad09066600c618d0c7b831f760c
Francisco Neto <netofrancisco.pro@gmail.com>
2025-08-18 22:41:18 -0300

    aula058 até aula061

---
+++ aulas/aula058.py
@@ -0,0 +1,10 @@
+
+# Interpretador do Python
+
+""" 
+python3 mod.py (executa um módulo)
+python3 -u (unbuffered; força a saída)
+python3 -m mod (lib mod como script)
+python3 -c 'print("testes");print(123)' (executa um comando, ; pode ser usado para separar comandos)
+python3 -i mod.py (interativo com mod) 
+"""
---
+++ aulas/aula059.py
@@ -0,0 +1,21 @@
+# Desempacotamento em chamadas de métodos e funções
+
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+
+salas = [
+    ['Maria', 'Helena', ],
+    ['Elaine', ],
+    ['Luiz', 'João', 'Eduarda', ],
+]
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista) # printa com o sep=' '
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
