db38c82ea6281c40bfdc27cdd1b8d97159d95e21
Francisco Neto <netofrancisco.pro@gmail.com>
2025-10-06 23:36:55 -0300

    aula 115 continuação

--- sec04_intermediario/aulas/aula115.py
+++ sec04_intermediario/aulas/aula115.py
@@ -1,25 +1,39 @@
 # Ambientes virtuais em Python (venv)
 
-# Um ambiente virtual carrega toda a sua instalação
-# do Python para uma pasta no caminho escolhido.
+""" Um ambiente virtual cria uma pasta isolada que usa o Python principal do 
+seu sistema como base, mas instala todas as bibliotecas de forma independente.
 
-# Ao ativar um ambiente virtual, a instalação do
-# ambiente virtual será usada.
+Ao ativar um ambiente virtual, a instalação do
+ambiente virtual será usada.
 
-# venv é o módulo que vamos usar para
-# criar ambientes virtuais.
+venv é o módulo que vamos usar para
+criar ambientes virtuais.
 
-# Você pode dar o nome que preferir para um
-# ambiente virtual, mas os mais comuns são:
-# venv env .venv .env
+Você pode dar o nome que preferir para um
+ambiente virtual, mas os mais comuns são:
+venv env .venv .env """
 
 
 # Como criar ambientes virtuais
 
-# Abra a pasta do seu projeto no terminal
-# e digite:
 # python3 -m venv venv
 
 
 # para saber o caminho do interpretador python
 # which python3
+
+
+# Ativando e desativando o ambiente virtual
+
+# source venv/bin/activate
+# deactivate
+
+
+# Instalando pacotes no ambiente virtual
+# pip install nome_do_pacote
+
+# pip freeze - lista os pacotes instalados com suas versões (lembre de estar com o ambiente virtual ativado)
+# pip freeze > requirements.txt - salva os pacotes instalados com suas versões no requirements.txt
+# pip list - lista os pacotes instalados para uma boa visualização
+# pip install -r requirements.txt - instala os pacotes listados no requirements.txt
+# pip uninstall nome_do_pacote - desinstala o pacote especificado
