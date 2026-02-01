388f327f9f844202f4d6e46e17f14abe0de228de
Francisco Neto <netofrancisco.pro@gmail.com>
2026-01-31 23:44:26 -0300

    aula 141 pt2

--- sec05_poo/aulas/aula141/log.py
+++ sec05_poo/aulas/aula141/log.py
@@ -2,18 +2,30 @@
 
 # Uma classe abstrata é uma classe que não deve ser instanciada diretamente.
 class Log:
-    def log(self, msg):  # assinatura do método
+    def _log(self, msg):  # assinatura do método
         raise NotImplementedError('Implemente o método log')
+
+    def log_error(self, msg):
+        self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        self._log(f'Success: {msg}')
 
 
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
 
 
 if __name__ == '__main__':
     # log = Log() # Vai gerar um erro, pois não podemos instanciar uma classe abstrata
     # log.log('Olá Mundo')
 
-    l = LogFileMixin()
-    l.log('qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_success('que legal')
