63a7bcdbbb546c861f41599daaf7b95b475b2fcf
Francisco Neto <netofrancisco.pro@gmail.com>
2026-03-20 20:08:57 -0300

    add aula 169

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from python-studies!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ sec06_modulos/aulas/aula169.py
@@ -0,0 +1,18 @@
+# os path
+
+import os
+
+file_path = os.path.join(
+    "sec06_modulos", "aulas", "aula169.py"
+)  # monta caminho relativo; resolvido a partir do cwd
+print(file_path)
+
+dire, file = os.path.split(file_path)  # faz uma tupla com o diretório e o arquivo
+print("Diretório:", dire)
+print("Arquivo:", file)
+
+print(os.path.exists(file_path))
+
+print(os.path.abspath("."))
+print(os.path.basename(file_path))
+print(os.path.dirname(file_path))
